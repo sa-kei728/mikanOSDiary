399c9f26b5256f67a98c3592abd73abdef85802d
sa-kei728 <keisuke.sato1@konicaminolta.com>
2023-08-07 10:16:07 +0900

    delete task directory

--- task/Day03/kernel/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-clang++ -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone \
--fno-exceptions -fno-rtti -std=c++17 \
--I/usr/include/c++/7 -I/usr/include/x86_64-linux-gnu/ -I/usr/include/x86_64-linux-gnu/c++/7/  \
--c main.cpp
-
-ld.lld --entry kernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o
--- task/Day04/boot_image_qemu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-qemu-system-x86_64 \
-    -drive if=pflash,file=OVMF_CODE.fd \
-    -drive if=pflash,file=OVMF_VARS.fd \
-    -hda disk.img \
-    -monitor stdio
